d8c70b3b1c549975999547c82ce2472e4bf5dda8
Eralex <eralex135@gmail.com>
2023-03-09 23:51:43 +0300

    ci

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.secret_key = 'secret'
 #         port=DBPORT,
 #         db=DBNAME)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:eralex@localhost:5432/postgres_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:eralex@localhost:5432/project_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
